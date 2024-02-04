bcd92e3e420d63568f0bbcfdbf9bfbe6c774782e
Mike Nykytenko <ur7ez@ukr.net>
2024-02-04 22:55:52 +0200

    initial commit

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800"/>
+            </Link>
+            LaraStorage
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown/>
+
+            <div class="py-3">
+                <NavLink href="/">My Files</NavLink>
+                <NavLink href="/" >Shared with Me</NavLink>
+                <NavLink href="/">Shared by Me</NavLink>
+                <NavLink href="/" >Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import CreateNewDropdown from "@/Components/app/CreateNewDropdown.vue";
+import NavLink from "@/Components/NavLink.vue";
+
+</script>
