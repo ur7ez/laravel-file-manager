28e5530e27a299e7fcbfc745b94a94bba4e6f0b6
Mike Nykytenko <ur7ez@ukr.net>
2024-02-13 16:00:51 +0200

    add to favourites feature

---
+++ app/Http/Requests/AddToFavouritesRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class AddToFavouritesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => Rule::exists('files', 'id')
+                ->where(function (Builder $query) {
+                    $query->where('created_by', Auth::id());
+                }),
+        ];
+    }
+}
